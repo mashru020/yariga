08f7489e13340c7e50d9c0ed7d16f4f5a9d7f25d
mashru020 <mashrurrahman16@gmail.com>
2023-03-13 23:34:27 +0600

    allproperties 1:20

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,7 @@ function App() {
             catchAll={<ErrorComponent />}
             resources={[
               {
-                name: "property",
+                name: "properties",
                 list: AllProperties,
                 show: PropertyDetails,
                 create: CreateProperty,
@@ -133,18 +133,18 @@ function App() {
                 icon: <VillaOutlined/>
               },
               {
-                name: "agent",
+                name: "agents",
                 list: Agents,
                 show: AgentProfile,
                 icon: <PeopleAltOutlined/>
               },
               {
-                name: "review",
+                name: "reviews",
                 list: Home,
                 icon: <StarOutlineRounded/>
               },
               {
-                name: "message",
+                name: "messages",
                 list: Home,
                 icon: <ChatBubbleOutlined/>                
               },
