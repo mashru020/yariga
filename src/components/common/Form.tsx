67bd6f8d9dd9252aa2f703e90fbd84da155fd488
mashru020 <mashrurrahman16@gmail.com>
2023-03-15 23:08:43 +0600

    from done 1:48 hour

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -82,7 +82,28 @@ const Form = ({type, register, handleSubmit, handleImageChange, formLoading, onF
             />
           </FormControl>
           <Stack direction="column" gap={1} justifyContent="center" mb={2}>
-            
+            <Stack direction='row' gap={2}>
+              <Typography color="#11142d" fontSize={16} fontWeight={500} my="10px">Property Photo</Typography>
+              <Button component='label'sx={{width:'fin-content', color:'#2ed480', textTransform:'capitalize', fontSize:16}}>
+                Upload *
+                <input
+                  hidden
+                  accept='image/*'
+                  type="file"
+                  onChange={(e)=>{
+                    // @ts-ignore
+                    handleImageChange(e.target.files[0])
+                  }}
+                />
+                </Button>
+            </Stack>
+            <Typography fontSize={14} color='#808191' sx={{wordBreak:'break-all'}}>{propertyImage?.name}</Typography>
+            <CustomButton 
+              type='submit'
+              title={formLoading ? 'Submittion...' : 'Submit'}
+              backgroundColor="#475beb"
+              color='#fcfcfc'
+            />
           </Stack>
         </form>
 
--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -1,18 +1,33 @@
 import { useState } from 'react';
 import { useGetIdentity } from '@pankod/refine-core';
-import { FieldValue, useForm } from '@pankod/refine-react-hook-form';
+import { FieldValue, FieldValues, useForm } from '@pankod/refine-react-hook-form';
 import { useNavigate } from '@pankod/refine-react-router-v6';
 
 import Form from 'components/common/Form';
+import { readFile } from 'fs';
+import { PropertyCard } from 'components';
 
+// kichu bujhini (1) => start (reading file and then reading data from it and set it in the state)
 const CreateProperty = () => {
   const navigate = useNavigate();
   const { data: user} = useGetIdentity();
   const [propertyImage, setPropertyImage] = useState({name:'', url:''})
   const { refineCore: {onFinish, formLoading}, register, handleSubmit} = useForm();
   
-  const handleImageChange = () => {}
-  const onFinishHandler = () => {}
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) => new Promise<string>((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile); 
+    });
+
+    reader(file).then((result: string) => setPropertyImage({name: file?.name, url:result}));
+  }
+// kichu bujhini (1) => end
+  const onFinishHandler = async (data: FieldValues) => {
+    if(!propertyImage.name) return alert('please select an image');
+    await onFinish({...data, photo: propertyImage.url, email: user.email})
+  }
   return (
     <Form
       type="Create"
