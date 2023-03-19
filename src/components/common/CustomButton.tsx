3d4d0001042a79ff958f2efb3dc2d33240b9cbaa
mashru020 <mashrurrahman16@gmail.com>
2023-03-19 23:33:15 +0600

    pagination and filtering layout 3:03 hour

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,9 +1,10 @@
 import { Button, minWidth, padding } from '@pankod/refine-mui'
 import { CustomButtonProps } from 'interfaces/common'
 
-const CustomButton = ({type, title, backgroundColor, color, fullWidth, icon, handleClick }:CustomButtonProps) => {
+const CustomButton = ({type, title, backgroundColor, color, fullWidth, icon, handleClick, disabled }:CustomButtonProps) => {
   return (
     <Button 
+      disabled={disabled}
       type= {type === 'submit' ? 'submit' : 'button'}
       sx={{
         flex: fullWidth ? 1 : 'unset',
